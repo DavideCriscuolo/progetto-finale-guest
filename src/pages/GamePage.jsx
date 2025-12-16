020c7d0b47018a62bcbe71f8efbe73c62bb516ae
DavideCriscuolo <davide.alcatel@gmail.com>
2025-12-16 21:36:19 +0100

    creato overlay per gioco

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -27,54 +27,45 @@ export default function GamePage() {
   return (
     <main>
       {load ? (
-        <div class="d-flex justify-content-center align-items-center my-5">
+        <div className="d-flex justify-content-center align-items-center my-5">
           <div
-            class="spinner-border text-success spinner-border-sm"
+            className="spinner-border text-success spinner-border-sm"
             role="status"
           ></div>
         </div>
       ) : (
-        <div className="container my-5">
-          <div className="row">
-            <div className="col d-flex">
-              <div className="col ">
-                <div>
-                  <img
-                    src={game.medias?.[0].url}
-                    className="img-fluid rounded"
-                    alt="img gioco"
-                  />
-                </div>
-              </div>
-              <div className="col">
-                <div class="card border-0">
-                  <div class="card-body text-black">
-                    <h4 class="card-title">{game.title}</h4>
-                    <span className="badge bg-danger">
-                      {game.category?.name}
-                    </span>
-                    {game.plattforms?.map((plat) => {
-                      return (
-                        <span className="badge bg-success">{plat.name}</span>
-                      );
-                    })}
-                    <p class="card-text">{game.plot}</p>
-                    <ul className="list-unstyled">
-                      <li class="card-text">Casa Editrice: {game.editor}</li>
-                      <li class="card-text">
-                        Classificazione: {game.classification}
-                      </li>
-                      <li class="card-text">
-                        Prezzo d'acquisto consigliato: {game.price}€
-                      </li>
-                      <li class="card-text">
-                        Anno D'uscita: {date.getFullYear()}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div>
+          <div className="jumboGame position-relative overflow-hidden">
+            {game.medias?.[0]?.url ? (
+              <img
+                className="w-100 h-100 object-fit-cover"
+                src={game.medias[0].url}
+                alt=""
+              />
+            ) : (
+              <div className="bg-secondary w-100 h-100" />
+            )}
+
+            <div className="overlay " />
+          </div>
+
+          <div className="p-4">
+            <h1 className="display-5 fw-bold">{game.title}</h1>
+            <p className="col-md-8 fs-4">{game.plot}</p>
+            <span className="badge bg-danger">{game.category?.name}</span>
+            {game.plattforms?.map((plat) => {
+              return <span className="badge bg-success">{plat.name}</span>;
+            })}
+            <ul className="list-unstyled">
+              <li className="card-text">Casa Editrice: {game.editor}</li>
+              <li className="card-text">
+                Classificazione: {game.classification}
+              </li>
+              <li class="card-text">
+                Prezzo d'acquisto consigliato: {game.price}€
+              </li>
+              <li class="card-text">Anno D'uscita: {date.getFullYear()}</li>
+            </ul>
           </div>
         </div>
       )}
